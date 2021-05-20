264b76fd8816899634797b88da0f4639fc233d72
longcat99 <33887488+longcat99@users.noreply.github.com>
2021-05-20 17:15:13 +0800

    Update tencentddns.lua

--- nb-luci/luci-app-tencentddns/luasrc/controller/tencentddns.lua
+++ nb-luci/luci-app-tencentddns/luasrc/controller/tencentddns.lua
@@ -1,6 +1,6 @@
 module("luci.controller.tencentddns",package.seeall)
 function index()
-    local page = entry({"admin", "ddns"}, firstchild(), "动态域名解析")
+    local page = entry({"admin", "ddns"}, firstchild(), "域名")
     page.order = 30
     page.dependent = false
     page.acl_depends = { "luci-app-tencentddns" }
